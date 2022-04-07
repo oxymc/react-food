47c68842b4588000f35e1091117a37bf826adeb8
Maxym Balagur <maxymbalagur@MacBook-Pro-Maxym.local>
2022-04-07 18:14:03 +0300

    fix light theme

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col l6 s12">
-            <h5 className="white-text"><NavLink to="/">Reciepes</NavLink></h5>
+            <h5 className="white-text"><NavLink to="/" className="grey-text text-lighten-4">Reciepes</NavLink></h5>
             <p className="grey-text text-lighten-4">
               Project was used React Routers and hooks
             </p>
@@ -14,8 +14,8 @@ const Footer = () => {
           <div className="col l4 offset-l2 s12">
             <h5 className="white-text">Links</h5>
             <ul>
-              <li><NavLink to="/about">About</NavLink></li>
-              <li><NavLink to="/contacts">Contacts</NavLink></li>
+              <li><NavLink to="/about" className="grey-text text-lighten-4">About</NavLink></li>
+              <li><NavLink to="/contacts" className="grey-text text-lighten-4">Contacts</NavLink></li>
             </ul>
           </div>
         </div>
