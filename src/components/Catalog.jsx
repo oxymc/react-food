74e74e879e71612df893aeb95da095dc48d81395
Maxym Balagur <maxymbalagur@MacBook-Pro-Maxym.local>
2022-04-08 17:31:40 +0300

    refactored result page v1.1

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -2,22 +2,22 @@ import { CategoryCard } from "./CategoryCard";
 import { NoResults } from "./NoResults";
 
 const Catalog = ({catalog = []}) => {
-  if(catalog.length) {
     return (
-      <div className="catalog">
-        {
-          catalog.map(
-            el => (
-              <CategoryCard key={el.idCategory} {...el} />
-            )       
-          )
-        }
-      </div>
+      <>
+        { catalog.length > 0 ?
+          <div className="catalog">
+            {
+              catalog.map(
+                el => (
+                  <CategoryCard key={el.idCategory} {...el} />
+                )       
+              )
+            }
+          </div> :
+          <NoResults />
+        }    
+      </>
     );
-  }
-  else {
-    return <NoResults />
-  }
 }
   
 export {Catalog};
